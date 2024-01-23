28d878ab71d36ca0de8a85e47a92726a5c856fa2
Prakash G <69726345+nalini06@users.noreply.github.com>
2024-01-23 21:15:52 +0530

    Changes

--- server/server.js
+++ server/server.js
@@ -21,13 +21,7 @@ app.use('/api/support-agents', supportAgentRoutes);
 app.use('/api/support-tickets', supportTicketRoutes);
 app.use('/api/tickets-agents', ticketAgentRoutes);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-}
 
 // Start the server
 
